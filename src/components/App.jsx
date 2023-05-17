f0cddddcc4f24c5a7152acb1ca345c9d1730cae1
danya-yablonskiy <danya.yablonskiy.00@gmail.com>
2023-05-17 15:20:27 +0300

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,74 +1,130 @@
-import { Component } from 'react';
+import { useEffect, useState } from 'react';
 import { nanoid } from 'nanoid';
 import { Form } from './Form/Form';
 import { Filter } from './Filter/Filter';
 import { ContactList } from './ContactList/ContactList';
 
-export class App extends Component {
-  state = {
-    contacts: [
-      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-    ],
-    filter: '',
-  };
+export const App = () => {
+  const [contacts, setContacts] = useState([
+    { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+    { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+    { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+    { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+  ]);
+  const [filter, setFilter] = useState('');
 
-  componentDidMount() { 
-    const localData = localStorage.getItem('contacts')
+  useEffect(() => {
+    const localData = localStorage.getItem('contacts');
     if (localData) {
-      this.setState({
-        contacts: JSON.parse(localData)
-      })
+      setContacts(JSON.parse(localData));
     }
-  }
+  }, []);
 
-  componentDidUpdate(_, prevState) {
-    if (prevState !== this.state.contacts) {
-      localStorage.setItem('contacts', JSON.stringify(this.state.contacts));
-    }
-  }
+  useEffect(() => {
+    localStorage.setItem('contacts', JSON.stringify(contacts));
+    console.log(contacts);
+  }, [contacts]);
 
-  addContact = (name, number) => {
-    const duplicate = this.state.contacts.find(
-      contact => contact.name === name
-    );
+  const addContact = (name, number) => {
+    const duplicate = contacts.find(contact => contact.name === name);
     if (duplicate) {
       alert('Error!');
       return;
     }
-    this.setState({
-      contacts: [...this.state.contacts, { id: nanoid(), name, number }],
-    });
-  };
-
-  changeFilter = e => {
-    this.setState({
-      filter: e.currentTarget.value,
-    });
-  };
-
-  deleteContact = id => {
-    const filterId = this.state.contacts.filter(contact => contact.id !== id);
-    this.setState({
-      contacts: [...filterId],
-    });
-  };
-
-  render() {
-    return (
-      <div>
-        <h1>Phonebook</h1>
-        <Form onSubmit={this.addContact} />
-        <h2>Contacts</h2>
-        <Filter onChange={this.changeFilter} value={this.state.filter} />
-        <ContactList
-          onClick={this.deleteContact}
-          contacts={this.state.contacts}
-          filter={this.state.filter}
-        />
-      </div>
+    setContacts(() => 
+      [...contacts, { id: nanoid(), name, number }]
     );
-  }
-}
+  };
+
+  const changeFilter = e => {
+    setFilter(e.currentTarget.value);
+  };
+
+  const deleteContact = id => {
+    const filterId = contacts.filter(contact => contact.id !== id);
+    setContacts([...filterId]);
+  };
+
+  return (
+    <div>
+      <h1>Phonebook</h1>
+      <Form onSubmit={addContact} />
+      <h2>Contacts</h2>
+      <Filter onChange={changeFilter} value={filter} />
+      <ContactList
+        onClick={deleteContact}
+        contacts={contacts}
+        filter={filter}
+      />
+    </div>
+  );
+};
+
+// export class App extends Component {
+//   state = {
+//     contacts: [
+// { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+// { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+// { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+// { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+//     ],
+//     filter: '',
+//   };
+
+//   componentDidMount() {
+//     const localData = localStorage.getItem('contacts')
+//     if (localData) {
+//       this.setState({
+//         contacts: JSON.parse(localData)
+//       })
+//     }
+//   }
+
+//   componentDidUpdate(_, prevState) {
+//     if (prevState !== this.state.contacts) {
+//       localStorage.setItem('contacts', JSON.stringify(this.state.contacts));
+//     }
+//   }
+
+// addContact = (name, number) => {
+//   const duplicate = this.state.contacts.find(
+//     contact => contact.name === name
+//   );
+//   if (duplicate) {
+//     alert('Error!');
+//     return;
+//   }
+//   this.setState({
+//     contacts: [...this.state.contacts, { id: nanoid(), name, number }],
+//   });
+// };
+
+// changeFilter = e => {
+//   this.setState({
+//     filter: e.currentTarget.value,
+//   });
+// };
+
+//   deleteContact = id => {
+//     const filterId = this.state.contacts.filter(contact => contact.id !== id);
+//     this.setState({
+//       contacts: [...filterId],
+//     });
+//   };
+
+//   render() {
+//     return (
+//       <div>
+//         <h1>Phonebook</h1>
+//         <Form onSubmit={this.addContact} />
+//         <h2>Contacts</h2>
+//         <Filter onChange={this.changeFilter} value={this.state.filter} />
+//         <ContactList
+//           onClick={this.deleteContact}
+//           contacts={this.state.contacts}
+//           filter={this.state.filter}
+//         />
+//       </div>
+//     );
+//   }
+// }
